f3e9ee18124f47dc62823f455bb9be33cb834e48
BorisRojnov <BorisRojnov@yandex.by>
2024-08-25 21:11:40 +0300

    Homework complete

---
+++ Task 1/src/Person.java
@@ -0,0 +1,59 @@
+public class Person {
+    private String name;
+    private String position;
+    private String job;
+    private int age;
+    private int salary;
+
+
+    public Person(){
+    }
+
+    public Person(String name, String position, String job, int age, int salary) {
+        this.name = name;
+        this.position = position;
+        this.job = job;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
--- Task 1/src/package2/Test3.java
+++ Task 1/src/package2/Test3.java
@@ -4,6 +4,6 @@ import package1.Test1;
 
 public class Test3 {
     public static void main(String[] args) {
-        Test1 test1 = new Test1();
+
     }
 }
